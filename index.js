514a6d1e9692db821211050b1bc66c9d7dc31d54
sandra <nyamweasandra@gmail.com>
2023-03-26 16:49:19 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+function  addingEventListener(){
+    const input = document.getElementById('button');
+    input.addEventListener('click', function(){
+        alert('i was clicked!');
+    })
+
 }
